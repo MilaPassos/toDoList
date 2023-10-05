8c2a5d56d3d415858794a4a2ece2a90d526e975a
MilaPassos <camila.pe.passos@hotmail.com>
2023-10-05 13:21:11 -0300

    v 0.1
    Adicionada uma validação de tipos de caracteres, tamanho máximo de letras e se há duplicidade.
    
    Adicionados botões para salvar e recuperar a lista do local storage

--- js/script.js
+++ js/script.js
@@ -1,19 +1,51 @@
 document.addEventListener("DOMContentLoaded", function () {
     var itemLista = document.querySelector("#itemList");
     var exibDiv = document.querySelector("#exibLista");
+    var saveBut = document.querySelector("#save");
+    var retrBut = document.querySelector("#retrieve");
+    var textoLista = "";
   
     document.querySelector("form").addEventListener("submit", function (event) {
+
         event.preventDefault();
 
         var itemAdc = itemLista.value;
         console.log(itemAdc);
 
-        adicionarLista(itemAdc);
+        if(listaComp.includes(itemAdc)){
+            alert ("Item já existente!")
+        }else{
+            adicionarLista(itemAdc);
+            var texto = itemAdc;
+            textoLista += texto + "<br>"
+        }
 
+        // for (i=0; i < listaComp.length; i++) {
+        // var texto = listaComp[i];
+        // textoLista += texto + "</br>";
+        // }
+
+        exibDiv.innerHTML = textoLista;
+      
         console.log(listaComp);
 
+        console.log(textoLista);
+    });
 
-        var textoLista = "";
+    saveBut.addEventListener("click", function(){
+        localStorage.setItem ("arraySalvo", listaComp);
+    });
+
+    retrBut.addEventListener("click", function (){
+        strgToArray = localStorage.getItem("arraySalvo");
+        listaComp = strgToArray.split(",")
+        console.log (listaComp)
+
+        if(listaComp !== null) {
+            alert("Lista recuperada");
+        }else{
+            alert("Nada a recuperar.")
+        }
 
         for (i=0; i < listaComp.length; i++) {
         var texto = listaComp[i];
@@ -21,21 +53,11 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         exibDiv.innerHTML = textoLista;
-      
-      });
+    })
 
-    });
-
-    var listaComp = [];
-
-    
+    var listaComp = []
 
     function adicionarLista (item){
-        listaComp.push(item)
-    };
-  
-  
-  
-  
-  
-  
+    listaComp.push(item)
+    }
+})
