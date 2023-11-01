098ed1abfb2ac9cd19235b37bf1557842eff7d19
MilaPassos <camila.pe.passos@hotmail.com>
2023-11-01 12:26:15 -0300

    Added some minor changes at the style and started to work on multiple lists
    - Added an input to list names
    - Some changes to fit the names list in the layout with Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,75 +1,85 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var itemLista = document.querySelector("#itemList");
-    var exibDiv = document.querySelector("#exibLista");
-    var saveBut = document.querySelector("#save");
-    var retrBut = document.querySelector("#retrieve");
-    var textoLista = "";
-  
-    document.querySelector("form").addEventListener("submit", function (event) {
+    
+    // Variáveis da lista de itens
+    var inputItem = document.querySelector("#itemList");
+    var arrayItens = document.querySelector("#showItens");
+    var saveButton = document.querySelector("#save");
+    var retrButton = document.querySelector("#retrieve");
+    var clearButton = document.querySelector("#clear");
+    var completeItensList = [];
+    var textList = "";
 
-        event.preventDefault();
+    // Variáveis da lista de listas
+    var inpuNameLists_ = document.querySelector("#inputList_");
+    var arrayListas_ = document.querySelector("#showList_");
+    var listSet_ = [];
+    var textList_ = "";
 
-        var itemAdc = itemLista.value;
-        console.log(itemAdc);
+    //Geral
+    const regex = /[0-9]|[\W]/;
+    var actualList = "";
 
-        if(listaComp.includes(itemAdc)){
+    document.querySelector("#send").addEventListener("click", function () {
+
+        var actualItem = inputItem.value;
+        console.log(actualItem);
+
+        if(completeItensList.includes(actualItem)){
             alert ("Item já existente!")
         }else{
-            adicionarLista(itemAdc);
-            var texto = itemAdc;
-            textoLista += texto + "<br>"
+            if(actualList !== ""){
+                addToList(actualItem, completeItensList);
+                refreshItensDisplay();
+                attachEditBtns();
+                attachDelBtns();
+                localStorage.setItem (actualList, completeItensList);
+            }else{
+                alert ("Crie uma lista primeiro!");
+            }
         }
-
-        // for (i=0; i < listaComp.length; i++) {
-        // var texto = listaComp[i];
-        // textoLista += texto + "</br>";
-        // }
-
-        exibDiv.innerHTML = textoLista;
       
-        console.log(listaComp);
+        console.log(completeItensList);
 
-        console.log(textoLista);
+        console.log(textList);
     });
 
-    saveBut.addEventListener("click", function(){
-        localStorage.setItem ("arraySalvo", listaComp);
+    saveButton.addEventListener("click", function(){
+        localStorage.setItem ("savedArray", completeItensList);
     });
 
-    retrBut.addEventListener("click", function (){
-        strgToArray = localStorage.getItem("arraySalvo");
-        listaComp = strgToArray.split(",")
-        console.log (listaComp)
+    retrButton.addEventListener("click", function (){
+        storageToArray = localStorage.getItem("savedArray");
+        completeItensList = storageToArray.split(",")
+        console.log (completeItensList)
 
-        if(listaComp !== null) {
+        if(completeItensList !== null) {
             alert("Lista recuperada");
         }else{
             alert("Nada a recuperar.")
         }
+        
+        refreshItensDisplay();
+        attachEditBtns();
+        attachDelBtns();
 
-        for (i=0; i < listaComp.length; i++) {
-        var texto = listaComp[i];
-        textoLista += texto + "</br>";
-        }
+    });
 
-        exibDiv.innerHTML = textoLista;
-    })
-
-    var listaComp = []
-
-    function adicionarLista (item){
-    listaComp.push(item)
+    clearButton.addEventListener("click", function(){
+        completeItensList = [];
+        refreshItensDisplay();
+    });
+    
+    function addToList (item, lista){
+    lista.push(item)
     }
-<<<<<<< Updated upstream
-=======
 
-    function atualizaDisplay () {
-        textoLista = "";
-        listaComp.forEach(function (item, index) {
+    function refreshItensDisplay () {
+        textList = "";
+        completeItensList.forEach(function (item, index) {
             var indexAt = index + 1
-            textoLista += `<span>${indexAt} - ${item}  </span><button class="editBtn"><img id="editImg" src="../Images/editBtn.png"></button><button class="delBtn"><img id="delImg" src="../Images/delBtn.png"></button><br>`
+            textList += `<span>${indexAt} - ${item}  </span><button class="editBtn"><img id="editImg" src="../Images/editBtn.png"></button><button class="delBtn"><img id="delImg" src="../Images/delBtn.png"></button><br>`
         })
-        exibDiv.innerHTML = textoLista;
+        arrayItens.innerHTML = textList;
     }
 
     function attachEditBtns () {
@@ -77,13 +87,13 @@ document.addEventListener("DOMContentLoaded", function () {
         editBtns = document.querySelectorAll(".editBtn");
         editBtns.forEach(function (item, index){
             item.addEventListener("click", function(){
-                var indexplus = index + 1
-                var editing = prompt("Editando o item número:" + indexplus);
+                var shownIndex = index + 1
+                var editing = prompt("Editando o item número:" + shownIndex);
                 if(editing.length>15 || regex.test(editing) === true || editing === null){
                     alert("Entrada inválida");
                 }else{
-                    listaComp.splice(index, 1, editing);
-                    atualizaDisplay();
+                    completeItensList.splice(index, 1, editing);
+                    refreshItensDisplay();
                     attachEditBtns();
                     attachDelBtns();
                 }
@@ -98,16 +108,55 @@ document.addEventListener("DOMContentLoaded", function () {
         var delBtns = [];
         delBtns = document.querySelectorAll(".delBtn");
         delBtns.forEach(function (item, index){
-            var indexplus = index + 1;
+            var shownIndex = index + 1;
             item.addEventListener("click", function(){
-                listaComp.splice(index, 1);
-                alert("Item número " + indexplus + " excluído");
-                atualizaDisplay();
+                completeItensList.splice(index, 1);
+                alert("Item número " + shownIndex + "excluído");
+                refreshItensDisplay();
                 attachEditBtns();
                 attachDelBtns();
             })
         })
     }
-        
->>>>>>> Stashed changes
+
+
+
+    document.querySelector(".listas_").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        var listaAdc = inpuNameLists_.value;
+
+        if(listSet_.includes(listaAdc)){
+            alert ("Já existe uma lista com esse nome!")
+        }else{
+            addToList(listaAdc, listSet_);
+            atualizaDisplayLista();
+        }
+
+        console.log(listSet_);
+    });
+    
+    document.listForm.onclick = function(){
+        actualList = document.querySelector('input[name = radioList]:checked').value;
+        console.log(actualList)
+    }
+
+    function atualizaDisplayLista () {
+        textList_ = "";
+        listSet_.forEach(function (item, index) {
+            var indexAt = index + 1
+            textList_ += `<input type="radio" name="radioList" id=${item} value=${item}> <span>${indexAt} - ${item}  </span><br>`
+        })
+        arrayListas_.innerHTML = textList_;
+
+        var value = $("input:radio[name=radioList]:checked").val()
+
+        if (value == null) {
+            $("input:radio[name=radioList]:first").prop("checked", true);
+            actualList = document.querySelector('input[name = radioList]:checked').value;
+            console.log(actualList)
+        }
+
+    }
+
 })
