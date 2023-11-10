e327dbc480e071c2cd7ecf09004a3508814259da
MilaPassos <camila.pe.passos@hotmail.com>
2023-11-10 20:33:03 -0300

    Implementada a função de criar várias listas, funcional mas com algumas correções a serem feitas

--- js/script.js
+++ js/script.js
@@ -1,23 +1,114 @@
+var listSet_ = [];
+var textList_ = "";
+var arrayListas_ = document.querySelector("#showList_");
+var actualListId = "";
+var actualList = "";
+var itemTocheck = "";
+var itensTitle = document.querySelector("#itensTitle");
+
+//Geral
+const regex = /[0-9]|[\W]/;
+
+
+
+//Save functions to lists
+function saveLists () {
+    localStorage.setItem("storageLists", listSet_);
+    console.log("Listas salvas");
+}
+function saveStorageCheckedId() {
+    actualListId = document.querySelector("input[name=radioList]:checked").id;
+    localStorage.setItem("listIdData", actualListId);
+}
+
+
+//Lists Buttons
+function attachEditListButtons () {
+    var editListButtons = [];
+    editListButtons = document.querySelectorAll(".editListBtn");
+    editListButtons.forEach(function (item, index){
+        item.addEventListener("click", function(){
+            var shownIndex = index + 1
+            var editingList = prompt("Editando a lista número: " + shownIndex);
+            if(editingList.length>15 || regex.test(editingList) === true || editingList === null){
+                alert("Entrada inválida");
+            }else{
+                listSet_.splice(index, 1, editingList);
+                refreshListsDisplay();
+                retrieveStorageCheckedData();
+                saveLists();
+            }
+        });
+    })
+};
+function attachDelListButtons (){
+    var delListButton = [];
+    delListButton = document.querySelectorAll(".delListBtn");
+    delListButton.forEach(function (item, index){
+        var shownIndex = index + 1;
+        item.addEventListener("click", function(){
+            listSet_.splice(index, 1);
+            localStorage.removeItem(`${actualList}`);
+            alert("Item número " + shownIndex + " excluído");
+            refreshListsDisplay();
+            retrieveStorageCheckedData();
+            saveLists();
+        })
+    })
+};
+
+
+function refreshListsDisplay () {
+    textList_ = "";
+    listSet_.forEach(function (item, index) {
+        var indexAt = index + 1
+        textList_ += `<input type="radio" name="radioList" class="radioList" id=${index} value=${item}> <span>${indexAt} - ${item} <button class="editListBtn"><img id="editImg" src="../Images/editBtn.png"></button><button class="delListBtn"><img id="delImg" src="../Images/delBtn.png"></button> </span><br>`
+    })
+    arrayListas_.innerHTML = textList_;
+    itensTitle.innerHTML = `Itens da lista ${actualList}:`;
+    attachDelListButtons();
+    attachEditListButtons();
+}
+
+function loadListsStorage () {    
+    var storageToArray = localStorage.getItem("storageLists");
+    listSet_ = storageToArray.split(",");
+
+    if(storageToArray !== null) {
+        refreshListsDisplay();
+        retrieveStorageCheckedData();
+        console.log("Lista recuperada");
+        console.log(listSet_);
+    }else{
+        alert("Não foi possível recuperar nenhuma lista.");
+    }
+};
+
+loadListsStorage();
+itensTitle.innerHTML = `Itens da lista ${actualList}:`;
+
+function retrieveStorageCheckedData() {
+    actualListId = localStorage.getItem("listIdData");
+    actualList = localStorage.getItem("listNameData")
+    console.log(actualListId);
+    itemTocheck = document.getElementById(`${actualListId}`);
+    if (itemTocheck){
+    itemTocheck.checked = true;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     
     // Variáveis da lista de itens
     var inputItem = document.querySelector("#itemList");
     var arrayItens = document.querySelector("#showItens");
-    var saveButton = document.querySelector("#save");
-    var retrButton = document.querySelector("#retrieve");
     var clearButton = document.querySelector("#clear");
     var completeItensList = [];
     var textList = "";
 
     // Variáveis da lista de listas
-    var inpuNameLists_ = document.querySelector("#inputList_");
-    var arrayListas_ = document.querySelector("#showList_");
-    var listSet_ = [];
-    var textList_ = "";
+    var inputNameLists_ = document.querySelector("#inputList_");
 
-    //Geral
-    const regex = /[0-9]|[\W]/;
-    var actualList = "";
 
     document.querySelector("#send").addEventListener("click", function () {
 
@@ -27,65 +118,42 @@ document.addEventListener("DOMContentLoaded", function () {
         if(completeItensList.includes(actualItem)){
             alert ("Item já existente!")
         }else{
-            if(actualList !== ""){
+            if(actualListId !== ""){
                 addToList(actualItem, completeItensList);
                 refreshItensDisplay();
-                attachEditBtns();
-                attachDelBtns();
-                localStorage.setItem (actualList, completeItensList);
+                saveItens();
             }else{
                 alert ("Crie uma lista primeiro!");
             }
         }
       
         console.log(completeItensList);
+        console.log(completeItensList.length);
 
         console.log(textList);
     });
-
-    saveButton.addEventListener("click", function(){
-        localStorage.setItem ("savedArray", completeItensList);
-    });
-
-    retrButton.addEventListener("click", function (){
-        storageToArray = localStorage.getItem("savedArray");
-        completeItensList = storageToArray.split(",")
-        console.log (completeItensList)
-
-        if(completeItensList !== null) {
-            alert("Lista recuperada");
-        }else{
-            alert("Nada a recuperar.")
-        }
-        
-        refreshItensDisplay();
-        attachEditBtns();
-        attachDelBtns();
-
-    });
-
-    clearButton.addEventListener("click", function(){
-        completeItensList = [];
-        refreshItensDisplay();
-    });
     
-    function addToList (item, lista){
-    lista.push(item)
-    }
+    retrieveItens();
 
+    //Itens display functions
     function refreshItensDisplay () {
         textList = "";
         completeItensList.forEach(function (item, index) {
-            var indexAt = index + 1
-            textList += `<span>${indexAt} - ${item}  </span><button class="editBtn"><img id="editImg" src="../Images/editBtn.png"></button><button class="delBtn"><img id="delImg" src="../Images/delBtn.png"></button><br>`
+            if (item != ""){
+                var shownIndex = index + 1
+                textList += `<span>${shownIndex} - ${item}  </span><button class="editItenBtn"><img id="editImg" src="../Images/editBtn.png"></button><button class="delItenBtn"><img id="delImg" src="../Images/delBtn.png"></button><br>`
+            }else{
+                completeItensList.splice(index,1);
+            }
         })
         arrayItens.innerHTML = textList;
+        attachEditItensButtons();
+        attachDelItensButtons();
     }
-
-    function attachEditBtns () {
-        var editBtns = [];
-        editBtns = document.querySelectorAll(".editBtn");
-        editBtns.forEach(function (item, index){
+    function attachEditItensButtons () {
+        var editItensButtons = [];
+        editItensButtons = document.querySelectorAll(".editItenBtn");
+        editItensButtons.forEach(function (item, index){
             item.addEventListener("click", function(){
                 var shownIndex = index + 1
                 var editing = prompt("Editando o item número:" + shownIndex);
@@ -94,69 +162,86 @@ document.addEventListener("DOMContentLoaded", function () {
                 }else{
                     completeItensList.splice(index, 1, editing);
                     refreshItensDisplay();
-                    attachEditBtns();
-                    attachDelBtns();
                 }
             });
-
-                // if(editing !== null){
-
         })
     }
-
-    function attachDelBtns (){
-        var delBtns = [];
-        delBtns = document.querySelectorAll(".delBtn");
-        delBtns.forEach(function (item, index){
+    function attachDelItensButtons (){
+        var delItensButton = [];
+        delItensButton = document.querySelectorAll(".delItenBtn");
+        delItensButton.forEach(function (item, index){
             var shownIndex = index + 1;
             item.addEventListener("click", function(){
                 completeItensList.splice(index, 1);
-                alert("Item número " + shownIndex + "excluído");
+                alert("Item número " + shownIndex + " excluído");
                 refreshItensDisplay();
-                attachEditBtns();
-                attachDelBtns();
+                saveItens();
             })
         })
     }
 
+    // Itens data management
+    function saveItens (){
+        localStorage.setItem (`${actualList}`, completeItensList);
+    }
+    function retrieveItens () {
+        var storageToArray = localStorage.getItem(`${actualList}`);
+        if (storageToArray !== null) {
+            completeItensList = storageToArray.split(",");
+        } else {
+            completeItensList = [];
+        }
+        console.log (completeItensList)
+
+        if(completeItensList !== null) {
+            console.log("Lista recuperada");
+        }else{
+            console.log("Nada a recuperar.");
+            completeItensList = [];
+        }
+    }
+    clearButton.addEventListener("click", function(){
+        completeItensList = [];
+        saveItens();
+        refreshItensDisplay();
+    });
 
 
-    document.querySelector(".listas_").addEventListener("submit", function (event) {
+
+    // Add an item to an List
+    function addToList (item, lista){
+        lista.push(item)
+    };
+
+
+    // Submit button for create lists
+    document.querySelector("#send_").addEventListener("click", function (event) {
         event.preventDefault();
 
-        var listaAdc = inpuNameLists_.value;
+        var newListInput = inputNameLists_.value;
 
-        if(listSet_.includes(listaAdc)){
+        if(listSet_.includes(newListInput)){
             alert ("Já existe uma lista com esse nome!")
         }else{
-            addToList(listaAdc, listSet_);
-            atualizaDisplayLista();
+            addToList(newListInput, listSet_);
+            refreshListsDisplay();
+            saveLists();
+            saveItens();
+            retrieveStorageCheckedData();
         }
 
         console.log(listSet_);
     });
     
+    // On click the lists radio buttons 
     document.listForm.onclick = function(){
         actualList = document.querySelector('input[name = radioList]:checked').value;
-        console.log(actualList)
-    }
-
-    function atualizaDisplayLista () {
-        textList_ = "";
-        listSet_.forEach(function (item, index) {
-            var indexAt = index + 1
-            textList_ += `<input type="radio" name="radioList" id=${item} value=${item}> <span>${indexAt} - ${item}  </span><br>`
-        })
-        arrayListas_.innerHTML = textList_;
-
-        var value = $("input:radio[name=radioList]:checked").val()
-
-        if (value == null) {
-            $("input:radio[name=radioList]:first").prop("checked", true);
-            actualList = document.querySelector('input[name = radioList]:checked').value;
-            console.log(actualList)
-        }
-
+        actualListId = document.querySelector('input[name = radioList]:checked').id;
+        localStorage.setItem("listIdData", actualListId);
+        localStorage.setItem("listNameData", actualList);
+        itensTitle.innerHTML = `Itens da lista ${actualList}:`;
+        retrieveItens();
+        refreshItensDisplay();
     }
 
 })
